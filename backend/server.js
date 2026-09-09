07c65fabeb5de641970dd794fc2736f2fbcc0163
Sibgat <sibgatofficial@gmail.com>
2026-09-09 17:41:28 +0000

    fix: update CORS configuration to specify allowed origin

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,11 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://3w-social-app-three.vercel.app",
+  }),
+);
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
